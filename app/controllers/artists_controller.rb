0d8accaac789428b3abe5ff1284f04bb0109a60c
luizfper <luizfper@gmail.com>
2019-07-01 15:06:42 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
-  def index
+	def index
+		@artists = Artist.all
   end
 
-  def show
+	def show
+		@artist = Artist.find(params[:id])
   end
 
   def new
@@ -40,7 +42,7 @@ class ArtistsController < ApplicationController
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
-  end
+	end
 
   private
 
