4efd614cc64c30f28283bede811bac8da1de461c
github username <github email address>
2019-07-03 20:58:33 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def artist_song_name
+    if self.artist
+      self.artist.name + " - " + self.title
+    end
+  end
 end
